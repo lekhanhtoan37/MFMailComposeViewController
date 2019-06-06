ae9d22c410f99bd286d0d42a7a6197c0097f569a
Lê Toàn <toanlk@paditech.org>
2019-06-06 13:12:06 +0700

    Send mail + Apps by DeveloperID

--- MFMailComposeViewController/ViewController.swift
+++ MFMailComposeViewController/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 import MessageUI
+import StoreKit
 
-class ViewController: UIViewController,MFMailComposeViewControllerDelegate {
+class ViewController: UIViewController,MFMailComposeViewControllerDelegate, SKStoreProductViewControllerDelegate {
   lazy var button: UIButton = {
     let view = UIButton()
     view.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+    return view
+  }()
+  
+  lazy var button2: UIButton = {
+    let view = UIButton()
+    view.frame = CGRect(x: 300, y: 100, width: 100, height: 100)
     return view
   }()
   
@@ -20,16 +27,56 @@ class ViewController: UIViewController,MFMailComposeViewControllerDelegate {
   let subject : String? = "Example"
   let messageBody : String? = "Reason"
   
+  let idDeveloper: String? = "526656015"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     self.view.addSubview(button)
+    self.view.addSubview(button2)
+
     button.tintColor = .red
     button.addTarget(self, action: #selector(sendEmail), for: .touchUpInside)
     button.setTitle("Send email", for: .normal)
     button.setTitleColor(.red, for: .normal)
     
+    button2.addTarget(self, action: #selector(getMoreApp), for: .touchUpInside)
+    button2.setTitle("More app", for: .normal)
+    button2.setTitleColor(.red, for: .normal)
   }
+  
+  @objc func getMoreApp() {
+    if #available(iOS 8.0, *) {
+      guard let idDeveloper = idDeveloper else {return}
+      openStoreProductWithiTunesItemIdentifier(identifier: idDeveloper)
+    } else {
+      let url  = NSURL(string: "itms://itunes.apple.com/vn/developer/king/id526656015?mt=8")
+      if UIApplication.shared.canOpenURL(url! as URL) == true  {
+        UIApplication.shared.openURL(url! as URL)
+      }
+      
+    }
+    
+  }
+
+  func openStoreProductWithiTunesItemIdentifier(identifier: String) {
+    let storeViewController = SKStoreProductViewController()
+    storeViewController.delegate = self
+    
+    let parameters = [ SKStoreProductParameterITunesItemIdentifier : identifier]
+    storeViewController.loadProduct(withParameters: parameters) { [weak self] (loaded, error) -> Void in
+      if loaded {
+        // Parent class of self is UIViewContorller
+        self?.present(storeViewController, animated: true, completion: nil)
+      }
+    }
+  }
+  func productViewControllerDidFinish(_ viewController: SKStoreProductViewController) {
+    viewController.dismiss(animated: true, completion: nil)
+  }
+  
+  
+  
   @objc func sendEmail() {
     let mailComposeViewController = configureMailComposer()
     if MFMailComposeViewController.canSendMail() {
@@ -75,6 +122,7 @@ class ViewController: UIViewController,MFMailComposeViewControllerDelegate {
     self.present(alertMessage, animated: true, completion: nil)
     
   }
+
 }
 
 
